bfdef1d693b152782c5472e0af5131bb9136cc8d
JamesBarryMusic <58055305+JamesBarryMusic@users.noreply.github.com>
2021-01-12 00:31:09 +0100

    Add files via upload

---
+++ app/functions.py
@@ -0,0 +1,93 @@
+import time
+from functools import wraps
+
+import pandas as pd
+
+from database.tools import Database
+
+
+def time_method(func):
+    @wraps(func)
+    def timed(*args, **kw):
+        ts = time.time()
+        result = func(*args, **kw)
+        te = time.time()
+
+        print("%r took %2.2f seconds to run." % (func.__name__, te - ts))
+
+        return result
+
+    return timed
+
+
+def get_audio_features():
+    return {
+        'valence': ['Valence', '#6fdc8c'],
+        'mode': ['Mode', '#08bdba'],
+        'energy': ['Energy', '#fa4d56'],
+        'danceability': ['Danceability', '#8a3ffc'],
+        'speechiness': ['Speechiness', '#bae6ff'],
+        'acousticness': ['Acousticness', '#007d79'],
+        'instrumentalness': ['Instrumentalness', '#fff1f1'],
+        'liveness': ['Liveness', '#d12771'],
+    }
+
+
+def load_data(chart, time_frame):
+    t_name = f"{chart.replace(' ', '').lower()}_{time_frame[0]}"
+    with Database('scores') as db:
+        scores_df = db.select(t_name)
+    scores_df.reset_index(drop=True, inplace=True)
+    scores_df.set_index(['date', 'country'], inplace=True)
+    country_meta = pd.read_csv('data/country_meta.csv')
+    country_meta.set_index('country', inplace=True)
+    scores_df = pd.merge(scores_df, country_meta[['name', 'iso3']], how='outer', left_index=True, right_index=True)
+    scores_df.reset_index(inplace=True)
+    scores_df['date'] = pd.to_datetime(scores_df['date'])
+    scores_df = scores_df.sort_values(by='date')
+    scores_df['date'] = scores_df['date'].apply(lambda x: x.strftime('%d/%m/%Y'))
+    return scores_df
+
+
+def country_geo_json(countries):
+    for i in range(len(countries['features'])):
+        countries['features'][i]['id'] = countries['features'][i]['properties']['iso_a3']
+    return countries
+
+
+def get_country_description(df, country='GBR'):
+    adverbs = {
+        1: '',
+        2: 'quite',
+        3: 'very',
+        4: 'super',
+    }
+    audio_expressions = {
+        'valence': ['happy', 1],
+        'mode': ['moody', -1],
+        'energy': ['energetic', 1],
+        'danceability': ['dancy', 1],
+        'speechiness': ['speechy', 1],
+        'acousticness': ['acoustic', 1],
+        'instrumentalness': ['instrumental', 1],
+        'liveness': ['live', 1],
+    }
+    country_series = \
+        df.loc[df.date == df.date.unique()[-1]].loc[df.iso3 == country][
+            [f's_{f}' for f in audio_expressions.keys()]].iloc[0]
+    descriptions = []
+    for feature, (adjective, sign) in audio_expressions.items():
+        val = country_series[f's_{feature}'] * sign
+        if abs(val) > 1:
+            adverb = ''
+            for level, a in adverbs.items():
+                if abs(val) > level:
+                    adverb = a
+            description = f'{adverb} {adjective}' if val > 0 else f'not {adverb} {adjective}'
+            description = description.replace('  ', ' ').strip()
+            descriptions.append(description)
+    if len(descriptions) == 0:
+        return 'Average'
+    else:
+        description_string = ', '.join(descriptions).capitalize()
+        return description_string
---
+++ app/layout.py
@@ -0,0 +1,102 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+from app.functions import get_audio_features, get_country_description
+from app.graph import get_map_figure, get_country_features_dist, get_country_features_ts, \
+    get_country_features_barchart
+
+SPOTIFY_LOGO = "assets/spotify_logo.png"
+
+graph_config = {
+    'displayModeBar': False
+}
+
+
+def get_layout(df):
+    layout = html.Div([
+        dbc.Row([
+            dbc.Col([
+                dbc.Navbar(
+                    [
+                        html.A(
+                            # Use row and col to control vertical alignment of logo / brand
+                            dbc.Row(
+                                [
+                                    dbc.Col(html.Img(src=SPOTIFY_LOGO, height="50px")),
+                                    dbc.Col(dbc.NavbarBrand("Spotify Geo Dashboard", className="ml-2")),
+                                ],
+                                align="center",
+                                no_gutters=True,)
+                        )
+                    ],
+                dark=True,
+                color = "black",)
+            ])
+        ], no_gutters=True),
+        dbc.Row([
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardBody([
+                        html.H1(id='country', children='United Kingdom'),
+                        html.P(id='country-description', className='lead', children=get_country_description(df)),
+                        html.Hr(),
+                        dbc.Row([
+                            dbc.Col([
+                                dcc.Graph(id='country-barchart', figure=get_country_features_barchart(df),
+                                          config=graph_config)
+                            ], width=6),
+                            dbc.Col([
+                                dcc.Graph(id='country-dist', figure=get_country_features_dist(df), config=graph_config)
+                            ], width=6),
+                        ]),
+                        html.Br(),
+                        dbc.Row([
+                            dbc.Col([
+                                dcc.Graph(id='country-ts', figure=get_country_features_ts(df), config=graph_config)
+                            ])
+                        ])
+                    ])
+                ])
+            ], width=6),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardBody([
+                        dcc.Graph(id='map', figure=get_map_figure(df), config=graph_config),
+                        dbc.Row([
+                            dbc.Col([
+                                dbc.Select(id='feature-dropdown',
+                                           options=[{'label': v[0], 'value': k} for k, v in
+                                                    get_audio_features().items()],
+                                           value='valence')
+                            ], width=2),
+                            dbc.Col([
+                                dcc.Slider(
+                                    id='map-slider',
+                                    min=0,
+                                    max=len(df.date.unique()) - 1,
+                                    value=len(df.date.unique()) - 1,
+                                    marks={
+                                        i: df.date.unique()[i] for i in range(len(df.date.unique()) - 1, -1, -32)
+                                    },
+                                    included=False,
+                                    updatemode='drag',
+                                )
+                            ], width=10),
+                            dbc.Col([
+                                html.H3(id='map-date')
+                            ], width=2)
+                        ]),
+                    ])
+                ]),
+                dbc.Card([
+                    dbc.CardBody([
+                        dcc.Markdown(open('INFO.md', 'r').read()),
+                    ])
+                ]),
+            ], width=6),
+        ], no_gutters=True),
+    ])
+
+
+    return layout
